2eed2fce0d58ac457e872e9f6becdee906a76440
Tim <orcatim@hotmail.com>
2022-09-11 11:46:57 +0000

    add filters to profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -31,5 +34,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             'content',
             'image',
             'is_owner',
-            'following_id'
+            'following_id',
+            'posts_count',
+            'followers_count',
+            'following_count'
         ]
