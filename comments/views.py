794bbd5fcffd59fad3047c7eaffb0c81cad8f4f8
Tim <orcatim@hotmail.com>
2022-09-13 08:37:04 +0000

    added and configured JWTs to project

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api_golf.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -8,6 +9,19 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+    ]
+
+    filterset_fields = [
+        'post',
+    ]
+
+    search_fields = [
+        'owner__username',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
